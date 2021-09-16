a94f792721781d482a011eaeb091cf426cb2189e
millin-stripe <millin@stripe.com>
2021-09-16 01:27:14 +0000

    Push from devtest

--- src/app.js
+++ src/app.js
@@ -1,27 +1,18 @@
-import { LightningElement } from "lwc";
+import { LightningElement } from 'lwc';
+import Stripe from 'stripe';
+import {loadStripe} from '@stripe/stripe-js';
 
+const publicstripe = await loadStripe('pk_test_51JIFtIDHKODBpsNtNDdhIXTgo7Crz0roTNBVqhw5PsGISQJWoT78gWZ0kUpt4HxdV4ggBmCCN3dEFdMXwWcyNcZZ00jRHudcOl');
+
+//console.log(publicstripe)
+const stripe = Stripe('sk_test_51JIFtIDHKODBpsNt0FCN5jBmVJ5UyJf83rtculsSrZXARLZfEc30jEmzJCUGrm0MygDP4Pf6P1bv0MAfZuFgSYzG00M3lzbREo');
+
+//console.log(stripe)
 export default class App extends LightningElement {
-  title = "Welcome to Lightning Web Components!";
-
-  showFeatures = true;
-
-  /**
-   * Getter for the features property
-   */
-  get features() {
-    return [
-      {
-        label: "Learn in the browser.",
-        icon: "utility:edit",
-      },
-      {
-        label: "View changes to code instantly with Live Compilation.",
-        icon: "utility:refresh",
-      },
-      {
-        label: "Style your components with SLDS.",
-        icon: "utility:brush",
-      },
-    ];
-  }
+   name = 'Electra X4';
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
 }
--- src/index.stories.js
+++ src/index.stories.js
@@ -2,5 +2,8 @@ import "@lwc/synthetic-shadow";
 import "https://unpkg.com/@salesforce-ux/design-system@2.13.1/assets/styles/salesforce-lightning-design-system.min.css";
 import { createElement } from "lwc";
 import App from "./app";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 export const story = () => createElement("c-app", { is: App });
